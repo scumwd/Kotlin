6934da04332e8f76e66f8ba41a6e1318d5308746
scumwd <dar102ruspro@gmail.com>
2021-10-12 01:06:37 +0300

    Completing homework for 15.10 1) Неявный Intent на использование камеры(заменяется фото аватара нажатимем на +). 2) Неявный Intent на принятие (image)файла из других приложений.

--- app/src/main/java/com/example/firsapp/MainActivity.kt
+++ app/src/main/java/com/example/firsapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.firsapp
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.EditText
 import android.widget.GridLayout
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,26 +32,55 @@ class MainActivity : AppCompatActivity() {
         R.drawable.photo4
     )
     private var index = 0
+    private val REQEST_TAKE_PHOTO = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         init()
-        var etName = findViewById<EditText>(R.id.et_name)
-        etName.setOnKeyListener(object : View.OnKeyListener {
+
+        binding.etName.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
                 if (event.action == KeyEvent.ACTION_DOWN &&
                     keyCode == KeyEvent.KEYCODE_ENTER
-                ) { etName.clearFocus()
-                    etName.isCursorVisible = false
+                ) { binding.etName.clearFocus()
+                    binding.etName.isCursorVisible = false
                     return true
                 }
                 return false
             }
         })
+        //intent
+        binding.ibTakepic.setOnClickListener {
+            val takePicIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePicIntent,REQEST_TAKE_PHOTO)
+        }
+
+        if (intent?.action==Intent.ACTION_SEND){
+            setPick(intent)
+        }
     }
+
+    private  fun setPick(intent: Intent){
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let{
+            val avatar = Drawable.createFromStream(contentResolver.openInputStream(it), it.toString())
+            binding.ivAvatar.setImageDrawable(avatar)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==REQEST_TAKE_PHOTO && resultCode== RESULT_OK){
+            (data?.extras?.get("data") as? Bitmap)?.let {
+                binding.ivAvatar.setImageBitmap(it)
+            }
+        }
+    }
+
 
     private fun init(){
         binding.apply {
