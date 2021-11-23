690b0ad6b889841532a27e76dd22e9ba3059b4d5
scumwd <ibneev2015@mail.ru>
2021-11-24 00:10:37 +0300

    1

--- app/src/main/java/com/example/firsapp/MainActivity.kt
+++ app/src/main/java/com/example/firsapp/MainActivity.kt
@@ -24,20 +24,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val setAlarm = findViewById<Button>(R.id.btn_start)
+        val stopAlarm = findViewById<Button>(R.id.btn_stop)
 
         val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         val hour = findViewById<EditText>(R.id.et_hour)
         val min = findViewById<EditText>(R.id.et_min)
         val intent = Intent(this, AlarmReciver::class.java)
-        val pendingIntent= PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE,intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         setAlarm.setOnClickListener {
+            val hour = if (!hour.text.toString().isEmpty())  hour.text.toString() else "0"
+            val min = if (!min.text.toString().isEmpty())min.text.toString() else "0"
+
+            val pendingIntent= PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE,intent, PendingIntent.FLAG_UPDATE_CURRENT)
             val calendar = Calendar.getInstance()
-            calendar.set(Calendar.HOUR,hour.text.toString().toInt())
-            calendar.set(Calendar.MINUTE, min.text.toString().toInt())
+            calendar.set(Calendar.HOUR,hour.toInt())
+            calendar.set(Calendar.MINUTE,min.toInt())
             val result: Long = calendar.timeInMillis
             alarmManager.set(AlarmManager.RTC_WAKEUP, result, pendingIntent)
         }
+        stopAlarm.setOnClickListener {
+                val pendingIntent = PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE, Intent(this, AlarmReciver::class.java), PendingIntent.FLAG_NO_CREATE)
+                pendingIntent?.cancel()
+        }
+
     }
 }
 
